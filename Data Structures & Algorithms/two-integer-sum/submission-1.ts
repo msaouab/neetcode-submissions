703eb420f501f6c4c20fcaecec4235315629cb16
Mohamed saouab <90860181+msaouab@users.noreply.github.com>
2026-07-17 23:45:21 +0100

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.ts
@@ -0,0 +1,20 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        const isSeen = new Map<number, number>();
+        // let i = 0
+        // while (i <= nums.length) {
+        for (let i = 0; i <= nums.length; i++) {
+            const c = target - nums[i];
+            if (isSeen.has(c)) {
+                return [isSeen.get(c)!, i];
+            }
+            isSeen.set(nums[i], i);
+            // i++;
+        }
+    }
+}
